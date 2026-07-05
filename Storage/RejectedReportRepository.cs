c6956abe75f8cce2c185ab778bc8940f2e78d573
Meir <meirroc5764@gmail.com>
2026-07-05 11:41:50 +0300

    finish regect Report

--- Storage/RejectedReportRepository.cs
+++ Storage/RejectedReportRepository.cs
@@ -1,5 +1,40 @@
-//using System;
-//namespace DataStorage
-//{
+using IntelligencePipeline.Models.Reports;
+using System;
+using System.Text.RegularExpressions;
+namespace IntelligencePipeline.Storage
+{
+    class RejectedReportRepository
+    {
+        private List<Report> _rejectedReports;
 
-//}
+        public RejectedReportRepository()
+        {
+            _rejectedReports = new List<Report>();
+        }
+        public void Add(Report report)
+        {
+            _rejectedReports.Add(report);
+        }
+        public List<Report> GetAll()
+        {
+            return _rejectedReports;
+        }
+        public int GetTotalCount()
+        {
+            return _rejectedReports.Count;
+        }
+        public List<Report> GetByReason(string reasonKeyword)
+        {
+            List<Report> result = new List<Report>();
+            foreach (Report regectReport in _rejectedReports)
+            {
+                if (Regex.IsMatch(regectReport.Description, $@"\b{keyword}\b", RegexOptions.IgnoreCase))
+                {
+                    result.Add(regectReport);
+                }
+            }
+
+            return result;
+        }
+    }
+}
